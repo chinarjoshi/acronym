3ea7675131679ab9065419f4c3ea4fe1682651ee
Chinar Joshi <chinarhjoshi@gmail.com>
2022-07-16 19:41:14 -0400

    Progress on tests by creating fixtures module, write documentation for main package.

--- acronym/acronym.py
+++ acronym/acronym.py
@@ -1,70 +1,74 @@
+"""Parse command line args and modify aliases.toml file."""
+
 import os
 import sys
-import toml
-from pathlib import Path
 from collections import defaultdict
+from pathlib import Path
 
-from acronym.alias_utils import *
+import toml
+
+import acronym.alias_utils as utils
 from acronym.help_text import HELP_TEXT
 
-ROOT = Path(__file__).parent.resolve() / 'data'
-SH = ROOT / 'aliases.sh'
-TOML = ROOT / 'aliases.toml'
-HELP = ROOT.parent / 'help.txt'
-KEYWORDS = ROOT / 'keywords.toml'
-FLAGS = '--flags', '--section'
-HISTFILE = os.environ.get('HISTFILE', False)
 
 
-# Parse the arguments, if register then add the word to the json file,
 def main():
-    with open(TOML) as f:
+    """Read aliases.toml file, modify it, and write it back to file.
+
+    This is the entrypoint of tool.
+    """
+    with open(utils.TOML) as f:
         aliases = defaultdict(dict, toml.load(f))
 
+    aliases = parse_args(aliases)
+
+    with open(utils.TOML, "w") as f, open(utils.SH, "w") as s:
+        toml.dump(aliases, f)
+        s.write(utils.generate_aliases(aliases))
+
+
+def parse_args(aliases: dict[str, dict[str, str]]) -> dict[str, dict[str, str]]:
+    """Parse command line args and modify given aliases dictionary.
+
+    Match sys.argv and look for enumerated commands and flags.
+
+    Args:
+        aliases (dict[str, dict[str, str]]): Aliases straight from aliases.toml file.
+
+    Returns:
+        Modified aliases dictionary.
+        If command is not "add", "rm", or "change", then return unmodified dict.
+    """
     match sys.argv[1:]:
-        case ['add', *cmd]:
-            for segment in ' '.join(cmd).split(','):
-                cmd = [w.strip(' \'"') for w in segment.split() if w not in FLAGS]
+        case ["add", *cmd]:
+            values = {k for x in aliases.values() for k in x}
+            for segment in " ".join(cmd).split(","):
+                ac = utils.Acronym.from_segment(segment)
+                utils.resolve_collisions(aliases, values, ac)
+                print(f'{ac.short} = "{ac.cmd_str}"')
 
-                section = cmd[0]
-                if 'sudo' in cmd:
-                    section = cmd[1]
-                if 'under' in cmd:
-                    section = cmd[cmd.index('under') + 1]
-                    cmd = cmd[:cmd.index('under')]
-
-                if 'as' in cmd:
-                    i = cmd.index('as')
-                    short = cmd[i + 1]
-                    cmd = cmd[:i]
-                else:
-                    filtered = [w for w in cmd if w != 'sudo']
-                    short = acronymize(filtered, use_flags='--flags' in segment)
-
-                ac = Acronym(cmd, ' '.join(cmd), short, section)
-                values = {k for x in aliases.values() for k in x}
-
-                resolve_collisions(aliases, values, ac)
-                print(f'{short} = "{ac.cmd_str}"')
-
-        case ['edit']:
-            os.system(f"{os.environ.get('EDITOR', 'vim')} {str(TOML)}")
+        case ["edit"]:
+            os.system(f"{os.environ.get('EDITOR', 'vim')} {str(utils.TOML)}")
             exit(0)
 
-        case ['rm', *names]:
-            filtered = [w for w in names if w not in FLAGS]
-            if '--section' in names:
+        case ["rm", *names]:
+            filtered = [w for w in names if w not in utils.FLAGS]
+            if "--section" in names:
                 for name in filtered:
                     if not aliases.pop(name, False):
                         print(f"Category '{name}' not found.")
             else:
                 for name in filtered:
-                    if not any(prefix.pop(name, False) for prefix in aliases.values() if name in prefix):
+                    if not any(
+                        prefix.pop(name, False)
+                        for prefix in aliases.values()
+                        if name in prefix
+                    ):
                         print(f"Alias '{name}' not found.")
-            
+
             aliases = {k: v for k, v in aliases.items() if v}
 
-        case ['change', old, 'with', new]:
+        case ["change", old, "with", new]:
             for section in aliases:
                 if old in section:
                     aliases[section][new] = aliases[section].pop(old)
@@ -73,60 +77,76 @@ def main():
                 print(f"Alias '{old}' not found to be replaced.")
                 exit(1)
 
-        case ['suggest']:
-            with open(HISTFILE) as f, open(KEYWORDS) as k:
-                history = [line[15:-1] if line[0] == ':' else line.strip() for line in f.readlines()[:1000]]
+        case ["suggest"]:
+            with open(utils.HISTFILE) as f, open(utils.KEYWORDS) as k:
+                history = [
+                    line[15:-1] if line[0] == ":" else line.strip()
+                    for line in f.readlines()[:1000]
+                ]
                 recs = toml.load(k)
 
-            recs = toml.dumps({k: v for k, v in recs.items() if k in history and k not in aliases})
+            recs = toml.dumps(
+                {k: v for k, v in recs.items() if k in history and k not in aliases}
+            )
             if recs:
-                print('The following aliases are reccomended based on your command history:' + '\n' + recs)
+                print(
+                    "The following aliases are reccomended based on your command history:"
+                    + "\n"
+                    + recs
+                )
             else:
-                print('No current reccomendations')
+                print("No current reccomendations")
 
-        case ['print', *prefixes] if prefixes:
-            print('\n' + toml.dumps({k: v for k, v in aliases.items() if k in prefixes}))
+        case ["print", *prefixes] if prefixes:
+            print(
+                "\n" + toml.dumps({k: v for k, v in aliases.items() if k in prefixes})
+            )
 
-        case ['print']:
-            print('\n' + toml.dumps(aliases))
+        case ["print"]:
+            print("\n" + toml.dumps(aliases))
 
-        case [('help' | '-h' | '--help')]:
+        case [("help" | "-h" | "--help")]:
             print(HELP_TEXT)
 
-        case ['install']:
-            content = f'. {str(Path(__file__).parent)}/data/aliases.sh'
-            home = Path(os.environ['HOME']).resolve()
+        case ["install"]:
+            content = f". {utils.SH}"
+            home = Path(os.environ["HOME"]).resolve()
 
-            shellrc = home / ('.bashrc' if 'bash' in os.environ['SHELL'] else '.zshrc')
-            if 'zsh' in os.environ['SHELL']:
+            default_rc = home / (
+                ".bashrc" if "bash" in os.environ["SHELL"] else ".zshrc"
+            )
+            if "zsh" in os.environ["SHELL"]:
                 content += f"\nfpath+={home / '.local/share/zsh/site-functions'}"
-            if os.environ.get('BASHRC', False):
-                shellrc = Path(os.environ['BASHRC'])
-            elif os.environ.get('ZSHRC', False):
-                shellrc = Path(os.environ['ZSHRC'])
 
-            if not shellrc.exists():
-                print(f'''\
-{shellrc} not found.
-Please provide either environmental variable (ZSHRC | BASHRC), for example:
-"ZSHRC=/home/c/.config/zsh/.zshrc acronym install"
-or manually add the following line to your shell config file:
+            provided_rc = input(
+                f'Provide absolute path to rc file, or type "None" to add the line yourself. \
+            [Default: {default_rc}]'
+            )
 
-{content}''')
-                exit(1)
-            with open(shellrc, 'a') as f:
-                print(f"Appending '{content}' to {shellrc}")
+            if provided_rc:
+                if provided_rc == "None":
+                    print(
+                        f"Add this line to your rc file to source the aliases.\n\n{content}"
+                    )
+                elif not Path(provided_rc).exists():
+                    print(
+                        f"Path does not exist. \
+                    Add this line to your rc file to source the aliases.\n\n{content}"
+                    )
+                    exit(1)
+            else:
+                provided_rc = default_rc
+
+            with open(provided_rc, "a") as f:
+                print(f"Appending '{content}' to {provided_rc}")
                 f.write(content)
 
         case [*_]:
-            print('Incorrect usage.')
+            print("Incorrect usage.")
             print(HELP_TEXT)
             exit(1)
 
-
-    with open(TOML, 'w') as f, open(SH, 'w') as s:
-        toml.dump(aliases, f)
-        s.write(generate_aliases(aliases))
+    return aliases
 
 
 if __name__ == "__main__":
--- tests/test_alias_utils.py
+++ tests/test_alias_utils.py
@@ -1,59 +1,21 @@
-import pytest
-from collections import defaultdict
-
-from acronym.alias_utils import Acronym, acronymize, resolve_collisions, generate_aliases
-
-
-@pytest.fixture
-def example_aliases():
-    return defaultdict(dict, {
-        'jupyter': {
-            'jn': 'jupyter notebook',
-            'jl': 'jupyter lab'
-        },
-        'pip': {
-            'pi': 'pip install',
-            'pu': 'pip uninstall'
-        }
-    })
-
-
-@pytest.fixture
-def example_aliases_output():
-    return defaultdict(dict, {
-        'jupyter': {
-            'jn': 'jupyter notebook',
-            'jl': 'jupyter lab'
-        },
-        'pip': {
-            'pi': 'pip install',
-            'pu': 'pip uninstall'
-        },
-        'git': {
-            'gr': 'git reset --hard'
-        }
-    })
-
-
-@pytest.fixture
-def example_command():
-    return ['git', 'reset', '--hard']
+import acronym.alias_utils as utils
+from tests.fixtures import example_command, example_aliases, example_aliases_output
 
 
 def test_acronymize(example_command):
-    assert acronymize(example_command) == 'gr'
-    assert acronymize(example_command, use_flags=True) == 'grh'
+    assert utils.acronymize(example_command) == 'gr'
+    assert utils.acronymize(example_command, use_flags=True) == 'grh'
 
 
 def test_resolve_collisions(example_aliases, example_aliases_output, example_command):
     values = {v for y in example_aliases.values() for v in y.values()}
-    ac = Acronym(
+    ac = utils.Acronym(
         command=example_aliases,
-        short=acronymize(example_command),
+        short=utils.acronymize(example_command),
         section=example_command[0],
         cmd_str=' '.join(example_command),
     )
-    resolve_collisions(example_aliases, values, ac)
+    utils.resolve_collisions(example_aliases, values, ac)
 
     assert example_aliases == example_aliases_output
 
@@ -63,4 +25,4 @@ def test_generate_aliases(example_aliases):
 alias "jl"="jupyter lab"
 alias "pi"="pip install"
 alias "pu"="pip uninstall"'''
-    assert generate_aliases(example_aliases) == alias_sh
+    assert utils.generate_aliases(example_aliases) == alias_sh
