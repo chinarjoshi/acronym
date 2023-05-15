81782d63f99ce331284c2230c1d6bf1e94b80c8d
Chinar Joshi <chinarjoshi7@gmail.com>
2023-05-15 16:40:23 -0400

    Start fresh.

--- acronym/__init__.py
+++
@@ -1,3 +0,0 @@
-'''A smart alias management system to shorten your shell commands.'''
-
-__version__ = '0.1.0'
--- tests/__init__.py
+++
@@ -1,5 +0,0 @@
-import sys
-from pathlib import Path
-
-path = Path(__file__).parent.parent.resolve() / 'acronym'
-sys.path.append(str(path))
