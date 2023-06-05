3391055c8bcb997f8fee541abf606da1967fbffd
Chinar Joshi <chinarjoshi7@gmail.com>
2023-06-05 19:24:44 -0400

    Create add and remove subcommand logic

--- src/parse_args.c
+++ src/parse_args.c
@@ -9,34 +9,99 @@ const char *argp_program_version = "0.1.0";
 const char *argp_program_bug_address = "chinarjoshi7@gmail.com";
 
 int global_parse_opt(int key, char *arg, struct argp_state *state) {
-    if (key != ARGP_KEY_ARG) {
-        return ARGP_ERR_UNKNOWN;
-    }
-
-    struct argp argp = {0};
-    if (strcmp(arg, "add") == 0) {
-        argp.options = add_options;
-        argp.parser = add_parse_opt;
-    } else if (strcmp(arg, "remove") == 0) {
-
-    } else if (strcmp(arg, "tree") == 0) {
-
-    } else if (strcmp(arg, "show") == 0) {
-
-    } else if (strcmp(arg, "edit") == 0) {
-
-    } else {
-        argp_error(state, "%s is not a valid command.", arg);
-        return 1;
+    Cli *cli = state->input; 
+    switch (key) {
+        case 'v':
+            if (arg) {
+                int verbosity = atoi(arg);
+                if (verbosity < 0 || verbosity > 3) {
+                    argp_error(state, "Verbosity must be between 0-3. Provided: %s", arg);
+                }
+                cli->verbosity = verbosity;
+            } else {
+                cli->verbosity = 1;
+            }
+            break;
+        case 'q':
+            global_parse_opt('v', "0", state);
+            break;
+        case ARGP_KEY_ARG:;
+            int argc = state->argc - state->next + 1;
+            char **argv = &state->argv[state->next - 1];
+            if (strcmp(arg, "add") == 0) {
+                cli->type = ADD; 
+                argp_parse(&add_argp, argc, argv, 0, 0, &cli->cmd.add);
+            } else if (strcmp(arg, "remove") == 0) {
+                cli->type = REMOVE; 
+                argp_parse(&remove_argp, argc, argv, 0, 0, &cli->cmd.remove);
+            } else if (strcmp(arg, "tree") == 0) {
+                cli->type = TREE; 
+                argp_parse(&tree_argp, argc, argv, 0, 0, &cli->cmd.tree);
+            } else if (strcmp(arg, "show") == 0) {
+                cli->type = SHOW; 
+                argp_parse(&show_argp, argc, argv, 0, 0, &cli->cmd.show);
+            } else if (strcmp(arg, "edit") == 0) {
+                cli->type = EDIT; 
+                argp_parse(&edit_argp, argc, argv, 0, 0, &cli->cmd.edit);
+            } else {
+                argp_error(state, "Subcommand not found: %s", arg);
+            }
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
     }
     return 0;
 }
 
 int add_parse_opt(int key, char *arg, struct argp_state *state) {
+    struct Add *add = state->input;
+    switch (key) {
+        case 'a':
+            if (!(add->alias_override = malloc(strlen(arg) + 1)))
+                return 1;
+            strcpy(add->alias_override, arg);
+            break;
+        case 's':
+            if (!(add->alias_override = malloc(strlen(arg) + 1)))
+                return 1;
+            strcpy(add->section_override, arg);
+            break;
+        case 'i':
+            add->include_flags = true;
+            break;
+        case 'l':
+            add->local = true;
+            break;
+        case ARGP_KEY_ARG:;
+            char *command = state->argv[state->next];
+            if (!(add->alias_override = malloc(strlen(command) + 1)))
+                return 1;
+            strcpy(add->alias_override, command);
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
     return 0;
 }
 
 int remove_parse_opt(int key, char *arg, struct argp_state *state) {
+    struct Remove *remove = state->input;
+    switch (key) {
+        case 'r':
+            remove->recursive = true;
+            break;
+        case 'f':
+            remove->force = true;
+            break;
+        case 'i':
+            remove->interactive = true;
+            break;
+        case 'l':
+            remove->local = true;
+            break;
+        case ARGP_KEY_ARG:
+            
+    }
     return 0;
 }
 int tree_parse_opt(int key, char *arg, struct argp_state *state) {
@@ -50,8 +115,7 @@ int edit_parse_opt(int key, char *arg, struct argp_state *state) {
 }
 
 struct Cli parse_args(int argc, char **argv) {
-    struct Cli cli;
-    struct argp global_argp = { 0, global_parse_opt }; 
+    Cli cli;
     argp_parse(&global_argp, argc, argv, 0, 0, &cli);
     return cli;
 }
--- src/parse_args.h
+++ src/parse_args.h
@@ -3,11 +3,6 @@
 #include <argp.h>
 #include <stdbool.h>
 
-const struct argp_option global_options[] = {
-    { "verbose", 'v', "VERBOSITY", OPTION_ARG_OPTIONAL, "Level of output verbosity (0-3)"},
-    { 0 }
-};
-
 struct Add {
     char *command; // Required
     char *alias_override;
@@ -16,28 +11,17 @@ struct Add {
     bool local;
 };
 
-const struct argp_option add_options[] = {
-    { "alias", 'a', "ALIAS", 0, "Optionally provide alias" },
-    { "section", 's', "SECTION", 0, "Optionally provide section/category" },
-    { "include-flags", 'i', 0, 0, "Include flags when generating default alias" },
-    { "local", 'l', 0, 0, "Change env file in current directory" },
-    { 0 }
-};
+typedef struct RemoveAliasNode {
+    RemoveAliasNode *next;
+    char *data;
+} RemoveAliasNode;
 
 struct Remove {
-    char **aliases; // Required
+    RemoveAliasNode *aliases;
     bool recursive;
     bool force;
     bool interactive;
     bool local;
-};
-
-const struct argp_option remove_options[] = {
-    { "recursive", 'r', 0, 0, "Remove a whole section instead of alias(es)" },
-    { "force", 'f', 0, 0, "Never prompt, even if alias not found" },
-    { "interactive", 'i', 0, 0, "Prompt before every removal" },
-    { "local", 'l', 0, 0, "Change env file in current directory" },
-    { 0 }
 };
 
 struct Tree {
@@ -46,32 +30,14 @@ struct Tree {
     bool all;
 };
 
-const struct argp_option tree_options[] = {
-    { "depth", 'L', "DEPTH", 0, "Max display depth of the tree" },
-    { "all", 'a', 0, 0, "Start from home directory" },
-    { 0 }
-};
-
 struct Show {
     char *directory;
     bool all;
 };
 
-const struct argp_option show_options[] = {
-    { "directory", 'd', "DIRECTORY", 0, "Directory to show active aliases" },
-    { "all", 'a', 0, 0, "Show all active aliases, not just overriden ones" },
-    { 0 }
-};
-
 struct Edit {
     char *editor;
     bool local;
-};
-
-const struct argp_option edit_options[] = {
-    { "editor", 'e', "EDITOR", 0, "Name of editor to use" },
-    { "local", 'l', 0, 0, "Edit env file in current directory" },
-    { 0 }
 };
 
 union Cmd {
@@ -84,17 +50,67 @@ union Cmd {
 
 enum CmdType { ADD, REMOVE, TREE, SHOW, EDIT };
 
-struct Cli {
+typedef struct Cli {
     union Cmd cmd;
     enum CmdType type;
     int verbosity;
+} Cli;
+
+const struct argp_option global_options[] = {
+    { "verbose", 'v', "VERBOSITY", OPTION_ARG_OPTIONAL, "Level of output verbosity (0-3)"},
+    { 0 }
 };
 
 int global_parse_opt(int key, char *arg, struct argp_state *state);
+const struct argp global_argp = { global_options, global_parse_opt }; 
+
+const struct argp_option add_options[] = {
+    { "alias", 'a', "ALIAS", 0, "Optionally provide alias" },
+    { "section", 's', "SECTION", 0, "Optionally provide section/category" },
+    { "include-flags", 'i', 0, 0, "Include flags when generating default alias" },
+    { "local", 'l', 0, 0, "Change env file in current directory" },
+    { 0 }
+};
+
 int add_parse_opt(int key, char *arg, struct argp_state *state);
+const struct argp add_argp = { add_options, add_parse_opt }; 
+
+const struct argp_option remove_options[] = {
+    { "recursive", 'r', 0, 0, "Remove a whole section instead of alias(es)" },
+    { "force", 'f', 0, 0, "Never prompt, even if alias not found" },
+    { "interactive", 'i', 0, 0, "Prompt before every removal" },
+    { "local", 'l', 0, 0, "Change env file in current directory" },
+    { 0 }
+};
+
 int remove_parse_opt(int key, char *arg, struct argp_state *state);
+const struct argp remove_argp = { remove_options, remove_parse_opt }; 
+
+const struct argp_option tree_options[] = {
+    { "depth", 'L', "DEPTH", 0, "Max display depth of the tree" },
+    { "all", 'a', 0, 0, "Start from home directory" },
+    { 0 }
+};
+
 int tree_parse_opt(int key, char *arg, struct argp_state *state);
+const struct argp tree_argp = { tree_options, tree_parse_opt }; 
+
+const struct argp_option show_options[] = {
+    { "directory", 'd', "DIRECTORY", 0, "Directory to show active aliases" },
+    { "all", 'a', 0, 0, "Show all active aliases, not just overriden ones" },
+    { 0 }
+};
+
 int show_parse_opt(int key, char *arg, struct argp_state *state);
+const struct argp show_argp = { show_options, show_parse_opt }; 
+
+const struct argp_option edit_options[] = {
+    { "editor", 'e', "EDITOR", 0, "Name of editor to use" },
+    { "local", 'l', 0, 0, "Edit env file in current directory" },
+    { 0 }
+};
+
 int edit_parse_opt(int key, char *arg, struct argp_state *state);
+const struct argp edit_argp = { edit_options, edit_parse_opt }; 
 
 #endif
