65cb674c8a8348d1022e38f997d53a943ab83bd5
Chinar Joshi <chinarjoshi7@gmail.com>
2023-08-09 04:48:28 -0400

    Add documentation and implement hash function

--- src/ACEntry.c
+++ src/ACEntry.c
@@ -7,16 +7,31 @@ Status create_ACEntry(ACEntry **data_out, const char *command,
     return SUCCESS;
 }
 
+// Returns the first word from 'command', allocating memory.
+// "git add -A" -> "git"
 char *create_section_name(const char *command) {
     return NULL;
 }
 
+// Returns an acronym for 'command', allocating memory.
+// An acronym is one word whose letters are the first letter of every word in
+// 'command' before the first optlon/flag, in order.
+// If 'include_flags', then options and the first letter of flags are included,
+// exluding their arguments, in order.
+// "ex subcmd -asd 3 --flag", false -> "es"
+// "ex subcmd -asd 3 --flag", true -> "esasdf"
 char *create_alias_name(const char *command, bool include_flags) {
     return NULL;
 }
 
-int hash_alias(char *alias, int capacity) {
-    return 0;
+// Returns the DJB2 hash of 'str' mod capacity.
+int hash_alias(char *str, int capacity) {
+    int hash = 5381;
+    while (str) {
+        hash = (hash << 5) + hash + *str;
+        str++;
+    }
+    return (hash & 0xFFFFFFFF) % capacity;
 }
 
 void free_ACEntry(ACEntry *ac) {
