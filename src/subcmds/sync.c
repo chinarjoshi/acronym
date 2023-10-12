e8edaf2812bc77e6afaead8c91f00672d3668d1e
Chinar Joshi <chinarjoshi7@gmail.com>
2023-10-12 18:34:04 -0400

    update sync

--- src/subcmds/sync.c
+++ src/subcmds/sync.c
@@ -7,18 +7,11 @@
 #include <time.h>
 #include "../file_io/file_io.h"
 
-static inline bool has_remote() { return system("git remote show"); }
-static inline bool init_git_repo() { return system("git init --quiet"); }
-static inline bool pull_changes() { return system("git pull --quiet"); }
-static inline bool push_changes() { return system("git push --quiet"); }
-static inline bool is_valid_git_repo() { return system(
-    "git rev-parse --is-inside-work-tree > /dev/null 2>&1"); }
-static inline bool add_and_commit(const char *message) {
-    char cmd[128];
-    snprintf(cmd, sizeof(cmd), "git add aliases.sh && git commit -m '%s'", message);
-    return system(cmd);
+static char _cmd_buf[256];
+static inline bool git_cmd(const char *cmd, const char *optarg) {
+    snprintf(_cmd_buf, sizeof(_cmd_buf), "git -qC %s %s %s", ACRONYM_DIRNAME, cmd, optarg);
+    return system(_cmd_buf);
 }
-void get_diff_stats(char *buffer, size_t buffer_size);
 
 bool sync_cmd(Cli *cli) {
     struct Sync s = cli->cmd.sync;
@@ -29,55 +22,49 @@ bool sync_cmd(Cli *cli) {
         return false;
     }
     // Ensure that it is a valid git repository
-    if (!is_valid_git_repo()) {
-        if (!init_git_repo()) {
+    if (!git_cmd("rev-parse --is-inside-work-tree", 0)) {
+        if (!git_cmd("init", 0)) {
             fprintf(stderr, "Error: Failed to initialize Git repository in %s.\n", ACRONYM_DIRNAME);
             return false;
         }
     }
 
-    char command[256];
-    command[0] = '\0';
     if (s.remote_URL) {
-        snprintf(command, sizeof(command), "git remote set-url origin %s", s.remote_URL);
-        if (system(command))
+        if (git_cmd("remote set-url origin", s.remote_URL))
             return false;
     } else if (s.forward) {
-        snprintf(command, sizeof(command), "git checkout HEAD@{%d}", s.forward);
-        if (system(command))
+        if (git_cmd("checkout HEAD@", s.forward))
             return false;
     } else if (s.backward) {
-        snprintf(command, sizeof(command), "git checkout HEAD~%d", s.forward);
-        if (system(command))
+        if (git_cmd("checkout HEAD~", s.backward))
             return false;
     } else if (s.commit_hash) {
-        snprintf(command, sizeof(command), "git checkout %s", s.commit_hash);
-        if (system(command) != 0) {
-            return false;  // Failed to checkout to the commit hash
-        }
+        if (git_cmd("checkout", s.commit_hash))
+            return false;
     }
 
     // No-args case
     else {
-        if (has_remote()) {
-            if (!pull_changes()) {
+        if (git_cmd("remote show", 0)) {
+            if (!git_cmd("pull", 0)) {
                 fprintf(stderr, "Error: Git pull failed.\n");
                 return false;
             }
         }
 
         // Add aliases.sh and commit
-        get_diff_stats(command, sizeof(command));
-        if (strlen(command) == 0) {
+        char diff_buf[256];
+        get_diff_stats(diff_buf, sizeof(diff_buf));
+        if (strlen(diff_buf) == 0) {
             return true; // Nothing to do
         }
-        if (!add_and_commit(command)) {
+        if (!git_cmd("add aliases.sh && git commit -m", diff_buf)) {
             fprintf(stderr, "Error: Git add and commit failed.\n");
             return false;
         }
 
-        if (has_remote()) {
-            if (!push_changes()) {
+        if (git_cmd("remote show", 0)) {
+            if (!git_cmd("push", 0)) {
                 fprintf(stderr, "Error: Git push failed.\n");
                 return false;
             }
@@ -121,5 +108,5 @@ void get_diff_stats(char *buffer, size_t buffer_size) {
     strftime(date, sizeof(date), "%b %d:", tm_info);
 
     // Combine date and git stats
-    snprintf(buffer, buffer_size, "%s %s", date, ptr);
+    snprintf(buffer, buffer_size, "%s: %s", date, ptr);
 }
